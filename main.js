7a6cbb6a7fbdce27011a642f2fe830c0ee014870
Abdugani0815 <abduvohidova863@gmail.com>
2024-08-13 20:43:58 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+// let box = 'Rashodbek';
+// box = 'Muhammadmuso';
+
+// let ism = 'Jasurbek';
+// console.log(box);
+// console.log(ism);
+
+let ism = prompt("Ismingizni kiriting!");
+let familiya = prompt("familiyangizni kiriting!");
+
+console.log("Ism: " + ism);
+console.log("Famimiliya: " + familiya);
